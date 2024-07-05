84c9d2a549afe379e7aaace406dc191839ffad0a
Daniel Seara <dsseara@gmail.com>
2024-07-05 11:53:23 -0500

    added class to do inference for 1D dynamics

--- inference/sociohydro2DInferer.py
+++ inference/sociohydro2DInferer.py
@@ -80,9 +80,9 @@ class SociohydroInfer2D():
         
         self.diff_method = diff_method
 
-    def differentiate(self, x, y, order, periodic, axis):
+    def differentiate(self, x, f, order, periodic, axis):
         if self.diff_method == "savgol":
-            dydx = savgol_deriv(x, y,
+            dfdx = savgol_deriv(x, f,
                                 window_length=5,
                                 polyorder=3,
                                 order=order,
@@ -93,7 +93,7 @@ class SociohydroInfer2D():
         else:
             raise NotImplementedError("Only derivative available is savgol")
 
-        return dydx
+        return dfdx
     
     def calc_features(self, ABt, x, y):
         # get all derivatives
@@ -139,40 +139,48 @@ class SociohydroInfer2D():
         ϕ0_D1 = ϕ0_D1[..., np.newaxis]
         ϕ0_D2 = ϕ0_D2[..., np.newaxis]
 
+        # div( φ0 grad(φa) - φa grad(φ0) )
         T_term   = (ABt_D2 * ϕ0) - (ABt * ϕ0_D2)
 
         # note that ∇ϕ1 ⋅ ∇ϕ2 requires summing over the -2 dimension
         dot = "...ij,...ij->...j"
+        
+        # div( φ0 φa grad(φa) )
         kii_term = (
             ABt * np.einsum(dot, ϕ0_D1, ABt_D1) +
             ϕ0 * np.einsum(dot, ABt_D1, ABt_D1) +
             ϕ0 * ABt * ABt_D2
         )
 
+        # div( φ0 φa grad(φb) )
         kij_term = (
             ABt * np.einsum(dot, ϕ0_D1, BAt_D1) +
             ϕ0 * np.einsum(dot, ABt_D1, BAt_D1) +
             ϕ0 * ABt * BAt_D2
         )
 
+        # div( φ0 φa grad(lap(φa)) )
         Γ_term = (
             ABt * np.einsum(dot, ϕ0_D1, ABt_D3) +
             ϕ0 * np.einsum(dot, ABt_D1, ABt_D3) +
             ϕ0 * ABt * ABt_D4
         )
 
+        # div( φ0 φa grad(φa^2) )
         νiii_term = (
             2 * ABt**2 * np.einsum(dot, ϕ0_D1, ABt_D1) + 
             4 * ϕ0 * ABt * np.einsum(dot, ABt_D1, ABt_D1) + 
             2 * ϕ0 * ABt**2 * ABt_D2
         )
-
+        
+        # div( φ0 φa grad(φa φb) )
         νiij_term = (
             ABt * (BAt * np.einsum(dot, ϕ0_D1, ABt_D1) + ABt * np.einsum(dot, ϕ0_D1, BAt_D1)) + 
             ϕ0 * (BAt * np.einsum(dot, ABt_D1, ABt_D1) + ABt * np.einsum(dot, ABt_D1, BAt_D1)) +
             ϕ0 * ABt * (BAt * ABt_D2 + 2 * np.einsum(dot, ABt_D1, BAt_D1) + ABt * BAt_D2)
         )
 
+        # div( φ0 φa grad(φb^2) )
         νijj_term = (
             2 * ABt * BAt * np.einsum(dot, ϕ0_D1, BAt_D1) + 
             2 * ϕ0 * BAt * np.einsum(dot, ABt_D1, BAt_D1) + 
@@ -294,3 +302,273 @@ class SociohydroInfer2D():
 
         return fitA, fitB, dAdt, dBdt, featA, featB, pearsonr_A, pearsonr_B
 
+
+class SociohydroInfer1D():
+    """
+    Class used to perform linear regression on 1D time series data
+    to fit to sociohydrodynamic model with generic quadratic utility
+    functions. Assumes 2 groups.
+
+    Input
+    -----
+    ϕ1 : list[array-like]
+        list of fields for first group. Each element of list is has 3 dimensions,
+        (t, y, x). If time dimension is not at -1, pass t_dim to 
+    ϕ2 : list[array-like]
+        list of fields for second group. Each element of list is has 3 dimensions,
+        (t, y, x). If time dimension is not at -1, pass t_dim to 
+    x : list[array-like]
+        list of x-positions of fields. Each element of list is a 1D array-like
+    t : list[array-like]
+        list of time points of fields. Each element of list is a 1D array-like
+    t_dim : int (optional)
+        location of t-dimension of data, to be moved to last position
+    diff_method : str
+        string determining how to take derivatives. Options are
+        ["savgol", "spline", "spectral", "gauss"].
+        note: only savgol currently implemented
+
+    Attributes
+    ----------
+    ABts : list[array]
+        combined arrays of ϕ1's and ϕ2's
+    xs : list[array]
+        equal to input, x
+    ts : list[array]
+        equal to input, t
+    diff_method : str
+        equal to input, diff_method
+
+    """
+    
+    diff_method_opts = ["savgol", "spline", "spectral", "gauss"]
+    "options for differentiation methods"
+
+    def __init__(self,
+                 ϕ1: list,
+                 ϕ2: list,
+                 x: list,
+                 t: list,
+                 t_dim: int = 0,
+                 diff_method: str = "savgol",
+                 periodic: bool = False):
+        
+        if np.any([not isinstance(arg, list) for arg in [ϕ1, ϕ2, x, t]]):
+            raise TypeError("fields need to be given as lists")
+            # we require fields to be given as a list where
+            # each element comes from a specific region/county
+        
+        self.n_regions = len(ϕ1)
+        
+        # make sure time is last dimension
+        self.ABts = [np.stack([np.moveaxis(fi1, t_dim, -1),
+                               np.moveaxis(fi2, t_dim, -1)],
+                               axis=-1) for fi1, fi2 in zip(ϕ1, ϕ2)]
+        self.xs = x
+        self.dxs = [eks[1] - eks[0] for eks in x]
+        self.ts = t
+        self.dts = [tee[1] - tee[0] for tee in t]
+        self.periodic = periodic
+
+        if diff_method.lower() not in self.diff_method_opts:
+            raise ValueError(f"diff_method needs to be in {self.diff_method_opts}")
+        
+        self.diff_method = diff_method
+
+    def differentiate(self, f, x, order, periodic, axis):
+        if self.diff_method == "savgol":
+            dfdx = savgol_deriv(x, f,
+                                window_length=5,
+                                polyorder=3,
+                                order=order,
+                                periodic=periodic,
+                                axis=axis,
+                                smooth=True,
+                                smooth_polyorder=2)
+        else:
+            raise NotImplementedError("Only derivative available is savgol")
+
+        return dfdx
+    
+    def calc_features(self, ABt, x):
+        # get all derivatives
+        ABt_d1 = self.differentiate(x, ABt, order=1, periodic=self.periodic, axis=0)  # ∂/∂x
+        ABt_d2 = self.differentiate(x, ABt, order=2, periodic=self.periodic, axis=0)  # ∂^2/∂x^2
+        ABt_d3 = self.differentiate(x, ABt, order=3, periodic=self.periodic, axis=0)  # ∂^3/∂x^3
+        ABt_d4 = self.differentiate(x, ABt, order=4, periodic=self.periodic, axis=0)  # ∂^4/∂x^4
+
+        # flip A and B for cross-terms
+        BAt = np.flip(ABt, axis=-1)
+        BAt_d1 = np.flip(ABt_d1, axis=-1)
+        BAt_d2 = np.flip(ABt_d2, axis=-1)
+        # BAt_d3 = np.flip(ABt_d3, axis=-1)
+        # BAt_d4 = np.flip(ABt_d4, axis=-1)
+
+        # vacancies
+        ϕ0 = 1 - ABt.sum(axis=-1)
+        ϕ0 = ϕ0[..., np.newaxis]
+        ϕ0_d1 = self.differentiate(x, ϕ0, order=1, periodic=self.periodic, axis=0)
+        ϕ0_d2 = self.differentiate(x, ϕ0, order=2, periodic=self.periodic, axis=0)
+
+
+        ϕ0_D1 = ϕ0_D1[..., np.newaxis]
+        ϕ0_D2 = ϕ0_D2[..., np.newaxis]
+
+        # ∂x( φ0 ∂x φi - φi ∂x φ0 )
+        T_term   = (ABt_d2 * ϕ0) - (ABt * ϕ0_d2)
+
+        # ∂x( φ0 φa ∂x(φa) )
+        kii_term = (
+            (ϕ0_d1 * ABt * ABt_d1) +
+            (ϕ0 * ABt_d1 * ABt_d1) +
+            (ϕ0 * ABt * ABt_d2)
+        )
+        
+        # ∂x( φ0 φa ∂x(φb) )
+        kij_term = (
+            (ϕ0_d1 * ABt * BAt_d1) +
+            (ϕ0 * ABt_d1 * BAt_d1) +
+            (ϕ0 * ABt * BAt_d2)
+        )
+
+        # ∂x(φ0 φa ∂xxx(φa))
+        Γ_term   = (
+            (ϕ0_d1 * ABt * ABt_d3) +
+            (ϕ0 * ABt_d1 * ABt_d3) +
+            (ϕ0 * ABt * ABt_d4)
+        )
+
+        # ∂x( φ0 φa ∂x(φa^2) )
+        νiii_term = (
+            (ϕ0_d1 * ABt    * (2 * ABt * ABt_d1)) + 
+            (ϕ0    * ABt_d1 * (2 * ABt * ABt_d1)) +
+            (ϕ0    * ABt    * (2 * (ABt_d1**2 + ABt * ABt_d2)))
+        )
+        
+        # ∂x( φ0 φa ∂x(φa φb) )
+        νiij_term = (
+            (ϕ0_d1 * ABt    * (ABt_d1 * BAt + ABt * BAt_d1)) +
+            (ϕ0    * ABt_d1 * (ABt_d1 * BAt + ABt * BAt_d1)) +
+            (ϕ0    * ABt    * (ABt_d2 * BAt + 2 * ABt_d1 * BAt_d1 + ABt * BAt_d2))
+        )
+        
+        # ∂x( φ0 φa ∂x(φb^2) )
+        νijj_term = (
+            (ϕ0_d1 * ABt    * (2 * BAt * BAt_d1)) +
+            (ϕ0    * ABt_d1 * (2 * BAt * BAt_d1)) +
+            (ϕ0    * ABt    * (2 * (BAt_d1**2 + BAt * BAt_d2)))
+        )
+
+        features = np.stack([T_term, kii_term, kij_term, Γ_term,
+                            νiii_term, νiij_term, νijj_term], axis=-1)
+
+        return features
+    
+
+    def test_train_split(self, train_pct):
+
+        # assume that all fields occur at the same time so
+        # we assume that train/test split occurs at same times
+        # for all
+        train = np.random.choice(len(self.ts[0]),
+                                 int(len(self.ts[0]) * train_pct),
+                                 replace=False)
+        test = np.array([i for i in np.arange(len(self.ts[0])) if i not in train])
+
+        dAdt_train = []
+        dAdt_test = []
+        dBdt_train = []
+        dBdt_test = []
+        featA_train = []
+        featA_test = []
+        featB_train = []
+        featB_test = []
+        
+        # loop over all datasets
+        for ABt, x, t in zip(self.ABts, self.xs, self.ts):
+            features = self.calc_features(ABt, x)
+            nfeat = features.shape[-1]
+            ABt_dt   = self.differentiate(t, ABt, order=1, periodic=False, axis=-2)  # ∂/∂t
+
+            # ∂ϕA/∂t
+            At_dt = ABt_dt[..., 0]
+            At_dt_notnan = np.all(~np.isnan(At_dt), axis=-1)
+            # features of A
+            fA = features[..., 0, :]
+            fA_notnan = np.all(~np.isnan(fA), axis=(-1, -2))
+            # ∂ϕB/∂t
+            Bt_dt = ABt_dt[..., 1]
+            Bt_dt_notnan = np.all(~np.isnan(Bt_dt), axis=-1)
+            # features of B
+            fB = features[..., 1, :]
+            fB_notnan = np.all(~np.isnan(fB), axis=(-1, -2))
+
+            notnan = np.logical_and(np.logical_and(At_dt_notnan, fA_notnan),
+                                    np.logical_and(Bt_dt_notnan, fB_notnan))
+
+            At_dt_train = At_dt[..., train][notnan].ravel()
+            At_dt_test  = At_dt[...,  test][notnan].ravel()
+            fA_train = np.reshape(fA[..., train, :][notnan], [len(At_dt_train), nfeat])
+            fA_test  = np.reshape(fA[..., test,  :][notnan], [len(At_dt_test),  nfeat])
+            Bt_dt_train = Bt_dt[..., train][notnan].ravel()
+            Bt_dt_test  = Bt_dt[...,  test][notnan].ravel()
+            fB_train = np.reshape(fB[..., train, :][notnan], [len(Bt_dt_train), nfeat])
+            fB_test  = np.reshape(fB[...,  test, :][notnan], [len(Bt_dt_test),  nfeat])
+
+            # append data to everything
+            # nanmask = np.logical_and(np.logical_and(np.all(~np.isnan(At_dt), axis=-1), np.all(~np.isnan(fA), axis=(-1, -2))),
+            #                          np.logical_and(np.all(~np.isnan(Bt_dt), axis=-1), np.all(~np.isnan(fB), axis=(-1, -2))))
+            # mask_train = np.all(~np.isnan(fA_train), axis=1)
+            # mask_test = np.all(~np.isnan(fA_test), axis=1)
+            
+            dAdt_train.append(At_dt_train)
+            dAdt_test.append(At_dt_test)
+            featA_train.append(fA_train)
+            featA_test.append(fA_test)
+            dBdt_train.append(Bt_dt_train)
+            dBdt_test.append(Bt_dt_test)
+            featB_train.append(fB_train)
+            featB_test.append(fB_test)
+        
+        dAdt = {"train": np.concatenate(dAdt_train),
+                "test" : np.concatenate(dAdt_test)}
+        dBdt = {"train": np.concatenate(dBdt_train),
+                "test" : np.concatenate(dBdt_test)}
+        
+        featA = {"train": np.concatenate(featA_train),
+                 "test" : np.concatenate(featA_test)}
+        featB = {"train": np.concatenate(featB_train),
+                 "test" : np.concatenate(featB_test)}
+
+        return dAdt, dBdt, featA, featB
+
+
+    def fit(self, train_pct, regressor="linear", alpha=0.1):
+        dAdt, dBdt, featA, featB = self.test_train_split(train_pct)
+
+        if regressor.lower() not in ['linear', 'elastic', 'sgd', 'lasso']:
+            raise ValueError("Regressor must be one of ['linear', 'elastic', 'sgd', 'lasso']. Currently: " + regressor)
+        
+        if regressor.lower() == "linear":
+            regA = lm.LinearRegression()
+            regB = lm.LinearRegression()
+        elif regressor.lower() == "elasticnet":
+            regA = lm.ElasticNet(alpha=alpha, l1_ratio=0.5)
+            regB = lm.ElasticNet(alpha=alpha, l1_ratio=0.5)
+        elif regressor.lower() == "sgd":
+            regA = lm.SGDRegressor(loss="squared_error")
+            regB = lm.SGDRegressor(loss="squared_error")
+        elif regressor.lower() == "lasso":
+            regA = lm.Lasso(alpha=alpha)
+            regB = lm.Lasso(alpha=alpha)
+
+        # perform fit
+        fitA = regA.fit(featA["train"], dAdt["train"])
+        fitB = regB.fit(featB["train"], dBdt["train"])
+
+        # pearson correlation coefficient
+        pearsonr_A = stats.pearsonr(dAdt["test"], fitA.predict(featA["test"])).statistic
+        pearsonr_B = stats.pearsonr(dBdt["test"], fitB.predict(featB["test"])).statistic
+        
+
+        return fitA, fitB, dAdt, dBdt, featA, featB, pearsonr_A, pearsonr_B
