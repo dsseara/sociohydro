46d3987cae9d9c7f112ba69dbbe736817d2d3b99
Daniel Seara <dsseara@gmail.com>
2024-07-19 14:06:20 -0500

    added plotting routines

--- inference/infer_utils.py
+++ inference/infer_utils.py
@@ -2,6 +2,8 @@ from scipy import interpolate, signal, ndimage
 import numpy as np
 import h5py
 import string
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def spline_deriv(x, y, polyorder=3, order=1, axis=0, smoothing=0):
     tck = interpolate.splrep(x, y, k=polyorder, s=smoothing)
@@ -152,3 +154,84 @@ def get_data(file, year=1990, region="all"):
     ϕB = black / (1.1 * capacity)
 
     return ϕW, ϕB, x_grid, y_grid
+
+
+def plot_coeffs(coef_df, pearson_df,
+                savename="./",
+                coefs_true=None):
+    fig, ax = plt.subplots(dpi=144, figsize=(4,2))
+    demo_codes = coef_df["demo"].unique()
+    nfeat = len(coef_df.name.unique())
+
+    for demo_code, color, offset in zip(demo_codes,
+                                        ["C0", "C3"],
+                                        [-0.2, +0.2]):
+        demo_df = coef_df.loc[coef_df["demo"]==demo_code][["val", "name"]]
+        xvals = pd.Categorical(demo_df.name).codes+offset
+        
+        ax.plot(xvals, demo_df.val, ".", color=color, alpha=0.1)
+        ax.errorbar(np.arange(7)+offset,
+                    demo_df.groupby("name").mean().reset_index().val.values,
+                    yerr=demo_df.groupby("name").std().reset_index().val.values,
+                    fmt="o", color=color, capsize=5)
+
+
+    ax.set(xticks=range(7), xticklabels=pd.Categorical(coef_df.name).categories.values,
+        ylabel="values", xlabel="coefficients");
+    for n in range(1, len(coef_df.name.unique())):
+        ax.axvline(n - 0.5, color="0.7")
+
+    ax.axhline(0, color="0.95", zorder=-1)
+    
+    if coefs_true is not None:
+        ax.plot(np.arange(nfeat) - 0.2, coefs_true[0],
+                "s", mfc="white", mec="C0", zorder=-1)
+        ax.plot(np.arange(nfeat) + 0.2, coefs_true[1],
+                "s", mfc="white", mec="C3", zorder=-1)
+
+    pW = pearson_df.loc[pearson_df.demo == "W"]
+    pB = pearson_df.loc[pearson_df.demo == "B"]
+
+    axp = ax.inset_axes([1.3, 0, 0.2, 1])
+    axp.plot([0] * len(pW), pW.coef, "C0.", alpha=0.1)
+    axp.errorbar([0], pW.coef.mean(),
+                 yerr=pW.coef.std(),
+                 fmt="o", color="C0", capsize=5)
+    axp.plot([1] * len(pB), pB.coef, "C3.", alpha=0.1)
+    axp.errorbar([1], pB.coef.mean(),
+                 yerr=pB.coef.std(),
+                 fmt="o", color="C3", capsize=5)
+    axp.set(xlim=[-0.5, 1.5],
+            xticks=[],
+            ylim=[-1, 1],
+            yticks=[-1, 0, 1],
+            ylabel="pearson coeff")
+    
+    fig.savefig(savename + "_inferredCoefs.pdf", bbox_inches="tight")
+
+
+def plot_regression(fit1, fit2, d1dt, d2dt, feat1, feat2,
+             savename="./"):
+    fig, ax = plt.subplots(dpi=144, figsize=(3, 3))
+    ax.plot(d1dt["test"],
+            fit1.predict(feat1["test"]),
+            "C0.", alpha=0.1, ms=1)
+    ax.plot(d2dt["test"],
+            fit2.predict(feat2["test"]),
+            "C3.", alpha=0.1, ms=1)
+
+    ax.ticklabel_format(axis="both", style="sci", scilimits=(0, 0), useMathText=True)
+    ax.set(
+        xlabel=r"$\dot{\phi}_i$ true",
+        ylabel=r"$\dot{\phi}_i$ prediction",
+        xlim=[1.1 * np.min([d1dt["test"].min(), d2dt["test"].min()]),
+            1.1 * np.max([d1dt["test"].max(), d2dt["test"].max()])],
+        ylim=[1.1 * np.min([d1dt["test"].min(), d2dt["test"].min()]),
+            1.1 * np.max([d1dt["test"].max(), d2dt["test"].max()])]
+    )
+
+    ax.axline([0, 0], slope=1, ls="--", color="0.8", zorder=-1)
+
+    ax.set_aspect(1)
+    fig.savefig(savename + "_regressionFit.pdf", bbox_inches="tight")
+    fig.savefig(savename + "_regressionFit.jpg", bbox_inches="tight")
